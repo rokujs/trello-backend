3d366ce4b5ebfed5e7b6e4c4953e05cf330d6ff8
rokujs <shiuxcode@gmail.com>
2023-03-03 11:28:47 -0500

    Update urls of state

--- trello_backend/apps/tasks/urls.py
+++ trello_backend/apps/tasks/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from apps.tasks import views
-from apps.tasks.views.state_view import StateCreateApiView, StateListApiView, StateDestroyApiView
 
 urlpatterns = [
     path("", views.TaskApiView.as_view(), name="tasks"),
@@ -16,9 +15,7 @@ urlpatterns = [
     path("priority/<int:pk>/",
          views.PriorityUpdateDestroyApiView.as_view(), name="priority_retrieve_update_destroy"),
 
-    path("state/", StateListApiView.as_view(), name="state_list"),
-    path("state/create_update/", StateCreateApiView.as_view(),
-         name="state_create_update"),
-    path("state/retrieve_destroy/<int:pk>/",
-         StateDestroyApiView.as_view(), name="state_retrieve_destroy")
+    path("state/", views.StateListCreateApiView.as_view(), name="state_list"),
+    path("state/<int:pk>/",
+         views.StateUpdateDestroyApiView.as_view(), name="state_retrieve_update_destroy")
 ]
--- trello_backend/apps/tasks/views/state_view.py
+++ trello_backend/apps/tasks/views/state_view.py
@@ -2,21 +2,14 @@ from rest_framework import generics
 from apps.tasks.serializers.state_serializer import StateSerializer
 
 
-class StateListApiView(generics.ListAPIView):
+class StateListCreateApiView(generics.ListCreateAPIView):
     serializer_class = StateSerializer
 
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.all()
 
 
-class StateCreateApiView(generics.UpdateAPIView, generics.CreateAPIView):
-    serializer_class = StateSerializer
-
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.all()
-
-
-class StateDestroyApiView(generics.RetrieveDestroyAPIView):
+class StateUpdateDestroyApiView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StateSerializer
 
     def get_queryset(self):
